67443fcd444271b3117698e354ae03ddbb4e6f6a
Williams392 <williamsvalle392@gmail.com>
2023-09-18 10:51:06 -0500

    mejorando events

--- core/events/models.py
+++ core/events/models.py
@@ -1,8 +1,32 @@
 # Estructura de la app movil Events:
+import uuid
 
 from django.db import models
 from fundamentals.models import Club
 from authentication.models import Profile
+
+from fundamentals.models import Currency
+from django.core.validators import MinValueValidator
+
+# Nuevo:
+class VisibiltyOptions(models.Model):
+    name = models.CharField(max_length=60)
+
+    def __str__(self):
+        return self.name
+
+class StatusOptions(models.Model):
+    name = models.CharField(max_length=60)
+
+    def __str__(self):
+        return self.name
+    
+class EventTypeOptions(models.Model):
+    name = models.CharField(max_length=60)
+
+    def __str__(self):
+        return self.name
+#
 
 class Events(models.Model):
     title = models.CharField(max_length=100, null=False, blank=False)
@@ -39,10 +63,38 @@ class Interested (models.Model):
     def __str__(self): # Nombre del Perfil - Título del Evento
         return f"{self.profile} - {self.event}"
     
-    def __str__(self):
-        return self.id_profile
+    #def __str__(self):
+        #return self.id_profile
     
 
+# Nuevo:
+class TicketType(models.Model):
+    event = models.ForeignKey(Events, on_delete=models.CASCADE)
+    name = models.CharField(max_length=250)
+    price = models.DecimalField(max_digits=100, decimal_places=2)
+    currency = models.ForeignKey(Currency, on_delete=models.PROTECT, null=True, blank=True)
+    max_number_to_issue = models.PositiveIntegerField(validators=[MinValueValidator(1)] ,null=True, blank=True)
+    remaining_tickets = models.PositiveIntegerField(validators=[MinValueValidator(0)], null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.event} - {self.name}"
+
+class Ticket(models.Model):
+    ticket_type = models.ForeignKey(TicketType, on_delete=models.CASCADE)
+    assigned_to = models.ForeignKey(Profile, on_delete=models.PROTECT)
+    code = models.UUIDField(default = uuid.uuid4, editable = False)
+    scan_done = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.ticket_type} - {self.assigned_to}"
+
+    def save(self, *args, **kwargs):
+        if not self.pk:  # Si se está creando un nuevo objeto
+            print(self.ticket_type.remaining_tickets)
+            self.ticket_type.remaining_tickets = self.ticket_type.remaining_tickets - 1 # Generar una clave aleatoria
+            self.ticket_type.save()
+        super().save(*args, **kwargs)
+#
 
 # comando -> [python manage.py makemigrations}
 # comando -> [python manage.py migrate}
--- core/events/views.py
+++ core/events/views.py
@@ -12,10 +12,19 @@ class EventDetailView(APIView):
 
     parser_classes = [JSONParser]
 
-    def get(self, request, event_id, format=None):
-        event = get_object_or_404(Events, pk=event_id) 
-        serializer = EventsSerializer(event)
+    def get(self, request, format=None):
+        queryset = Events.objects.all()
+        serializer = EventsSerializer(queryset, many=True)
         return Response(serializer.data)
+
+    # para crear un nuevo:
+    def post(self, request, format=None):
+        serializer = EventsSerializer(data=request.data)
+        
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=201)
+        return Response(serializer.errors, status=400)
 
 class EventList(APIView):
     parser_classes = [JSONParser]
